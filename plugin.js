b873d33da58ce37be7763c9feeca5276c11eea94
WebbuDesign <info@webbudesign.com>
2014-04-18 02:15:33 +0300

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,43 @@
+(function($) {
+"use strict";   
+ 
+    tinymce.init({
+        selector: ".wp-editor-area",
+        toolbar: "shortcodes",
+        setup: function(editor) {
+			
+			
+ 			//Shortcodes
+            tinymce.PluginManager.add( 'zillaShortcodes', function( editor, url ) {
+				
+				editor.addCommand("zillaPopup", function ( a, params )
+				{
+					var popup = params.identifier;
+					tb_show("Insert Zilla Shortcode", url + "/popup.php?popup=" + popup + "&width=" + 800);
+				});
+     
+                editor.addButton( 'zilla_button', {
+                    type: 'splitbutton',
+                    icon: false,
+					title:  'Zilla Shortcodes',
+					onclick : function(e) {},
+					menu: [
+					{text: 'Alerts',onclick:function(){
+						editor.execCommand("zillaPopup", false, {title: 'Alerts',identifier: 'alert'})
+					}},
+					{text: 'Buttons',onclick:function(){
+						editor.execCommand("zillaPopup", false, {title: 'Buttons',identifier: 'buttons'})
+					}},
+					//List your shortcodes like this
+					]
+					
+                
+        	  });
+         
+          });
+         
+        }
+         
+    });
+ 
+})(jQuery);
